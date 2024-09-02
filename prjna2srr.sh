277c46e5c0173ad22092803a56755b17be8760e2
Akihiro Kuno <akikuno@users.noreply.github.com>
2024-09-02 14:28:14 +0900

    Support both `wget` and `curl`

--- prjna2srr.sh
+++ prjna2srr.sh
@@ -4,23 +4,44 @@ set -e
 ACCESSION=$1
 ACCESSION_ID="${ACCESSION#PRJNA}"
 
-SRX=$(wget -qO - "https://www.ncbi.nlm.nih.gov/sra?linkname=bioproject_sra_all&from_uid=${ACCESSION_ID}" |
+if [ -z "$ACCESSION" ]; then
+    echo "Error: Please provide a PRJNA ID"
+    exit 1
+fi
+
+
+if type curl >/dev/null 2>&1; then
+    CMD_CURL='curl -s'
+elif type wget    >/dev/null 2>&1; then
+    CMD_CURL='wget -qO -'
+else
+    error_exit 1 'No HTTP-GET/POST command found.'
+fi
+
+
+SRX=$($CMD_CURL "https://www.ncbi.nlm.nih.gov/sra?linkname=bioproject_sra_all&from_uid=${ACCESSION_ID}" |
     tr "/" "\n" |
     grep ^SRX |
     head -n 1 |
     cut -d "[" -f 1)
 
-[ -z "$SRX" ] && echo "No SRX found for ${ACCESSION}" && exit 1
+if [ -z "$SRX" ]; then
+    echo "No SRX found for ${ACCESSION}"
+    exit 1
+fi
 
-SRP=$(wget -qO - https://www.ncbi.nlm.nih.gov/sra/${SRX} |
+SRP=$($CMD_CURL https://www.ncbi.nlm.nih.gov/sra/${SRX} |
     tr ">" "\n" |
     grep ^SRP |
     head -n 1 |
     cut -d "<" -f 1)
 
-[ -z "$SRP" ] && echo "No SRP found for ${ACCESSION}" && exit 1
+if [ -z "$SRP" ]; then
+    echo "No SRP found for ${ACCESSION}"
+    exit 1
+fi
 
-wget -qO - "https://ddbj.nig.ac.jp/resource/sra-study/${SRP}.jsonld" |
+$CMD_CURL "https://ddbj.nig.ac.jp/resource/sra-study/${SRP}.jsonld" |
     tr "/" "\n" |
     grep ^SRR |
     cut -d '"' -f 1 |
